3613cd6dfd7c41a2e2d0c76e0bec581ab6f4518f
seehos511-byte <seehos511@gmail.com>
2026-05-09 23:23:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 
+// Image Upload Preview
+const imgInput = document.getElementById('img-input');
+const charImg = document.getElementById('char-img');
+
+imgInput.addEventListener('change', function() {
+    const file = this.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            charImg.setAttribute('src', e.target.result);
+        }
+        reader.readAsDataURL(file);
+    }
+});
